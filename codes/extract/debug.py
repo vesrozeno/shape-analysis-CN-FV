5f70772b28aee10b0454a1bed615fc20d8d82788
vesrozeno <vtr.rozeno@gmail.com>
2024-10-02 09:01:11 -0300

    debug

--- codes/extract/debug.py
+++ codes/extract/debug.py
@@ -3,7 +3,6 @@ import numpy as np
 from ComplexNetwork import ComplexNetwork
 from FisherVectorEncoding import FisherVectorEncoding
 from sklearn.mixture import GaussianMixture
-from skimage.feature import fisher_vector
 
 # Inicializando listas para armazenar os descritores
 degrees = []
@@ -39,58 +38,46 @@ adjacency_matrices = [
     ]),
 ]
 
-adjacency_matrix = np.array([
-    [0, 0, 0],
-    [1, 0, 1],
-    [0, 1, 0]
-])
-
-grau,_,_ = CN.extract_features(adjacency_matrix)
-
-grau = grau.T
-
-print(f'Graus: \n{grau}')
-
-
-# Extração de características da rede complexa para todas as matrizes de adjacência
+# Iterando sobre as matrizes de adjacência
 for adj_matrix in adjacency_matrices:
     # Extraindo características (graus, forças, coeficiente de clustering)
     grade, force, cc = CN.extract_features(adj_matrix)
     
     # Armazenando os descritores extraídos
-    degrees.append(grade.T)
-    forces.append(force.T)
-    clustering.append(cc.T)
+    degrees.append(grade.T)  # Transpondo se necessário
+    forces.append(force.T)    # Transpondo se necessário
+    clustering.append(cc.T)    # Transpondo se necessário
 
-CNFeatures = np.concatenate([np.concatenate([d, f, c], axis=1) for d, f, c in zip(degrees, forces, clustering)], axis=0)
+# Concatenando todas as características extraídas
+# Usando a função combine_features para combinar conforme os parâmetros desejados
+CNFeatures = np.concatenate([
+    np.concatenate([d, f, c], axis=1) 
+    for d, f, c in zip(degrees, forces, clustering)
+], axis=0)
 
-print(f'Descritores - Graus: \n{degrees[0]}')
-print(f'Descritores - Graus: \n{len(degrees[0])}')
-print(f'CNFeatures: \n{CNFeatures[0]}')
-print(f'CNFeatures: \n{len(CNFeatures[0])}')
+# Verificando a saída dos descritores
+print(f'Descritores - Graus da primeira amostra: \n{degrees[0]}')
+print(f'Tamanho dos descritores da primeira amostra: {len(degrees[0])}')
+print(f'CNFeatures (todas as características concatenadas): \n{CNFeatures}')
+print(f'Tamanho de CNFeatures: {CNFeatures.shape}')
 
 # Configurações para o Fisher Vector Encoding
 fisher_encoding = FisherVectorEncoding(k)
 
-
+# Ajuste do modelo GMM usando as características extraídas
 gmm = GaussianMixture(n_components=k, random_state=42, covariance_type='diag')
 gmm.fit(CNFeatures)
 
-print(f"gmm means:\n{gmm.means_}")
-print(f"gmm covariances:\n{gmm.covariances_}")
-print(f"gmm weights:\n{gmm.weights_}")
+# Verificar a saída do GMM
+print(f"GMM - Means:\n{gmm.means_}")
+print(f"GMM - Covariances:\n{gmm.covariances_}")
+print(f"GMM - Weights:\n{gmm.weights_}")
 
-if grau.ndim == 1:
-    grau = grau.reshape(-1, 1)
-
-fv = np.array([fisher_vector(grau, gmm)])
+CNFeatures = CNFeatures.reshape(1, -1)
 
 # Computando os vetores de Fisher (apenas como exemplo, usando os mesmos descritores para treino e teste)
-# training_fvs, testing_fvs = fisher_encoding.compute_fisher_vectors(CNFeatures, CNFeatures)
+training_fvs,testing_fvs = fisher_encoding.compute_fisher_vectors(CNFeatures,CNFeatures)
 
-print(f'Vetor de Fisher (treino): \n{fv}')
-print(f'Vetor de Fisher (treino): \n{fv.shape}')
-
-# Verificar a saída da GMM e dos vetores de Fisher
-# print(f'Vetor de Fisher (treino): \n{training_fvs[0]}')
-# print(f'Vetor de Fisher (treino): \n{training_fvs.shape}')
+# Verificar a saída dos vetores de Fisher
+print(f'Vetor de Fisher (treino): \n{training_fvs[0]}')
+print(f'Tamanho do Vetor de Fisher (treino): {training_fvs[0].shape}')
